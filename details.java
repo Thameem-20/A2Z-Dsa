1c0ef9ad47c3a43761e83cb4db2a5c08b121159c
Thameem-20 <ahmedthameem20@gmail.com>
2024-08-29 23:01:41 +0530

    first commit

---
+++ details.java
@@ -0,0 +1,13 @@
+ublic class details{
+	public static void main(String[] args){
+    String name = "Thameem";
+    int age = 21;
+    long phone = 8867575821L;
+    
+    System.out.println("Name:"+name);
+    System.out.println("Age:"+age);
+    System.out.println("Phone:"+phone);
+    
+    }
+    }
+    
