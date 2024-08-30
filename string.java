01d4097c3e16d7ae088a3e98ac046563441a8a7b
Thameem-20 <ahmedthameem20@gmail.com>
2024-08-30 16:59:40 +0530

    updated

---
+++ string.java
@@ -0,0 +1,13 @@
+public class string{
+	public static void main(String[] args){
+    String name = "thameems \"is\" coding";
+    String firstname = "ahmad ";
+    String lastname = "thameem";
+    
+    System.out.println("length of the string is:"+name.length());
+    System.out.println("length of the string is:"+name.toUpperCase());
+    System.out.println("index of the string is:"+name.indexOf("is"));
+    System.out.println(firstname.concat(lastname));
+    System.out.println(name);
+    }
+    }
