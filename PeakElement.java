feeee72a9d60ba835b9831175db1827d93fe4dcc
Thameem-20 <ahmedthameem20@gmail.com>
2024-09-20 07:02:43 +0530

    updated

---
+++ PeakElement.java
@@ -0,0 +1,29 @@
+public class PeakElement {
+    int peakEle(int arr[], int n) {
+        if (n == 1)
+            return arr[0]; 
+        if (arr[0] >= arr[1])
+            return arr[0]; 
+        if (arr[n-1] >= arr[n-2])
+            return arr[n-1]; 
+        
+        for (int i = 1; i < n - 1; i++) {
+            if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1]) {
+                return arr[i]; 
+            }
+        }
+        
+        return -1; 
+    }
+
+    public static void main(String[] args) {
+        PeakElement peakFinder = new PeakElement();
+        
+        int arr[] = {1, 2, 5, 4, 3, 7, 1}; 
+        int n = arr.length; 
+        
+        int peak = peakFinder.peakEle(arr, n); 
+        
+        System.out.println("Peak element is: " + peak); 
+    }
+}
